b0e78f187a2d48dec1d2be2242a6ecb0a1cc32a9
Super user <Super user@TeamCity>
2023-09-07 01:59:45 +0000

    TeamCity change in 'Whatsapp Business Java Api' project: runners of 'Generate documentation' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -131,30 +131,58 @@ object WhatsappBusinessJavaApi_Build : BuildType({
         python {
             command = script {
                 content = """
-                    import subprocess
+                    import http.client
+                    import json
                     import os
+                    import subprocess
                     
-                    def get_nearest_tag(branch_name, commit_hash):
-                        subprocess.run(['git', 'config', '--global', '--add', 'safe.directory', '%teamcity.build.checkoutDir%'], 
+                    MARKETING_WEBSITE_URL = 'localhost'
+                    RELEASE_NOTES_FILENAME = 'release_notes.txt'
+                    DEFAULT_BRANCH_NAME = 'main'
+                    
+                    def get_latest_tag():
+                        subprocess.run(['git', 'config', '--global', '--add', 'safe.directory', '%teamcity.build.checkoutDir%'],
                                        stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
-                        result = subprocess.run(["git", "describe", "--tags", "--abbrev=0", branch_name, commit_hash],
+                        result = subprocess.run(["git", "describe", "--tags", "--abbrev=0", DEFAULT_BRANCH_NAME, 'HEAD'],
                                                  stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
                     
                         nearest_tag = result.stdout.strip()
                     
                         if not nearest_tag:
                             raise Exception("Tag not found!")
-                        
+                    
                         latest_tag = nearest_tag.split('\n')[-1]
-                        
+                    
                         return latest_tag
                     
+                    def get_release_notes(version):
+                        try:
+                            conn = http.client.HTTPConnection(MARKETING_WEBSITE_URL, port=80)
+                            conn.request("GET", "/releasenotes")
+                            response = conn.getresponse()
+                    
+                            if response.status == 200:
+                                response_data = response.read()
+                                json_data = json.loads(response_data.decode("utf-8"))
+                    
+                                return json_data[version]
+                            else:
+                                print(f"HTTP GET request failed with status code {response.status}")
+                                return None
+                    
+                        except Exception as e:
+                            print(f"An error occurred: {str(e)}")
+                            return None
+                    
+                    def write_notes(notes):
+                        with open(RELEASE_NOTES_FILENAME, 'w+') as f:
+                            f.write(notes)
+                    
                     if __name__ == "__main__":
-                        branch_name = "main"
-                        commit_hash = "HEAD"
-                        
-                        latest_tag = get_nearest_tag(branch_name, commit_hash)
-                        print(latest_tag)
+                        latest_tag = get_latest_tag(branch_name, commit_hash)
+                        release_notes = get_release_notes(latest_tag)
+                        if release_notes:
+                            write_notes(release_notes)
                 """.trimIndent()
             }
             dockerImage = "python:3.10"
