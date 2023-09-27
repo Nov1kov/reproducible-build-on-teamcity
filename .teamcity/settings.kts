af464653142c2f0c53fc43c9756fb64090d14cca
Super user <Super user@TeamCity>
2023-09-27 04:26:08 +0000

    TeamCity change in 'Whatsapp Business Java Api' project: runners of 'Check release notes' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -132,7 +132,7 @@ object WhatsappBusinessJavaApi_CheckReleaseNotes : BuildType({
             name = "Compare release notes"
             command = file {
                 filename = "ci.py"
-                scriptArguments = "--compare-release-notes"
+                scriptArguments = "--compare-release-notes --branch %teamcity.pullRequest.source.branch%"
             }
             dockerImage = "python:3.10"
             dockerImagePlatform = PythonBuildStep.ImagePlatform.Linux
